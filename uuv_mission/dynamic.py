edbc245497e89228ea3eccd487340e66a2cd4128
Tony <ton22.to83@gmail.com>
2024-10-21 15:56:12 +0100

    name change and autoreload extention added

--- uuv_mission/dynamic.py
+++ uuv_mission/dynamic.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from .terrain import generate_reference_and_limits
 import pandas as pd
-from controller import PDController
+from uuv_mission.controller import PDController
 
 class Submarine:
     def __init__(self):
